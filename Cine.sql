df83c982e956e09584adac65d129b6fc1c5afee4
Abraham Barrera <144285663+abrahambarrer@users.noreply.github.com>
2024-12-06 09:55:18 -0600

    Add files via upload

---
+++ Cine.sql
@@ -0,0 +1,5 @@
+INSERT INTO `Cine` (`cine_id`,`nombre_cine`,`estado`,`ciudad`,`codigo_postal`,`calle`,`colonia`,`numero_local`) VALUES  
+(1,'Cinépolis Acaya','Veracruz','Coatzacoalcos',96536,'Dr. José Lemarroy Carrión','Puerto Esmeralda',45) 
+(2,'Cinépolis El Dorado','Veracruz','Coatzacoalcos',96536,'Avenida Universidad Veracruzana','Las Gaviotas','151A') 
+(3,'Cinépolis Plaza Minatitlán','Veracruz','Minatitlán',96710,'Emiliano Zapata 10','Insurgentes Sur',10) 
+(4,'Cinépolis La Florida','Veracruz','Acayucan',96160,'Carretera Coatzacoalcos - Salina Cruz Km. 62 + 480','Colonia San José','NULL');
